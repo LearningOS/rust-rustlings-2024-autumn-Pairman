364c612ad11069a0deb5aad0b83c68bd8ee6777f
Pairman Guo <pairmanxlr@gmail.com>
2024-10-12 23:23:58 +0800

    update

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
-pub enum List {
-    Cons(i32, List),
+pub enum List<'a> {
+    Cons(i32, &'a List<'a>),
     Nil,
 }
 
@@ -34,12 +32,12 @@ fn main() {
     );
 }
 
-pub fn create_empty_list() -> List {
-    todo!()
+pub fn create_empty_list<'a>() -> List<'a> {
+    List::Nil
 }
 
-pub fn create_non_empty_list() -> List {
-    todo!()
+pub fn create_non_empty_list<'a>() -> List<'a> {
+    List::Cons(0, &List::Nil)
 }
 
 #[cfg(test)]
